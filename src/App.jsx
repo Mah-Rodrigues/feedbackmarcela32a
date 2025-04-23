ff5788821229083f90611e6eaa9d46f6b1651502
Marcela Rodrigues <marcelacosta2207@gmail.com>
2025-04-23 00:20:30 -0300

    criação do arquivo CardFinal.jsx para receber as informações do card final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
-import ilustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 import { CardInicial } from "./components/CardInicial"
+import { CardFinal } from "./components/CardFinal"
 
 export function App () {
   const [nota, setNota] = useState(0)
@@ -11,15 +10,7 @@ export function App () {
     submited === false ? (
       <CardInicial nota={nota} setNota={setNota} setSubmited={setSubmited} />
     ) : (
-      <div className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass text-center">
-        <img className="mx-auto mb-6" src={ilustrationThankYou} alt="ilustration Thank You" />
-
-        <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl text-sm">Você selecionou {nota} de 5 </p>
-
-        <h1 className="text-2xl font-bold mb-2.5">Obrigado!</h1>
-
-        <p className="text-sm text-light-grey mb-6">Agradecemos por você reservar um tempo para dar uma classificação. Se precisar de mais suporte, não hesite em entrar em contato!</p>
-      </div>
+      <CardFinal nota={nota} />
     )
   )
 }
